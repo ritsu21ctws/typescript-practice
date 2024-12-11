371b787d76e388c2262be6d0fd969842038ce8d8
ritsu <ritsu.21.ctws@gmail.com>
2024-12-11 19:03:10 +0900

    ユーザー一覧画面の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
 import React, { memo } from "react";
+import { Flex, HStack } from "@chakra-ui/react";
+import { UserCard } from "@/components/organisms/user/UserCard";
 
 export const UserManagement: React.FC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <HStack wrap='wrap' p={{ base: 4, md: 10 }}>
+      <Flex align='flex-start'>
+        <UserCard
+          imageUrl="https://picsum.photos/200"
+          userName="userName"
+          fullName="fullName"
+        />
+      </Flex>
+    </HStack>
+  );
 });
